c2f4aa235acc0a4196a9b8f334c6189405f56553
shlomi cohen <100487390+shlomi888c@users.noreply.github.com>
2023-02-27 17:20:25 +0200

    Update default.rb

--- default.rb
+++ default.rb
@@ -1,6 +1,6 @@
 execute 'sudo git clone https://shlomi888c@github.com/shlomi888c/projectschool.git' do
    cwd 'home'
 end
-execute 'sudo apt install python3-pip' do
+execute 'sudo apt install -y python3-pip' 
 # Install app dependencies
 execute 'sudo pip3 install -r /home/projectschool/requirements.txt'
