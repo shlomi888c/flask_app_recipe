b271a4f62aa7da3b1ec6f4afbc5fc97c8cb79a71
shlomi cohen <100487390+shlomi888c@users.noreply.github.com>
2023-06-10 21:30:46 +0300

    Update default.rb

--- default.rb
+++ default.rb
@@ -3,9 +3,12 @@ execute 'sudo git clone https://shlomi888c@github.com/shlomi888c/projectschool.g
 end
 execute  'sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb'
 execute  'sudo apt install -y ./google-chrome-stable_current_amd64.deb'
-execute  'sudo git clone https://shlomi888c@github.com/shlomi888c/chromedriver.git'
-execute  'sudo chmod +x /chromedriver/chromedriver'
-execute  'sudo mv /chromedriver/chromedriver /usr/local/bin/chromedriver'
+execute  'version=$(curl -s "https://chromedriver.storage.googleapis.com/LATEST_RELEASE")'
+execute  'wget -qP /tmp/ "https://chromedriver.storage.googleapis.com/${version}/chromedriver_linux64.zip"'
+execute  'sudo apt-get install unzip -y'
+execute  'sudo unzip /tmp/chromedriver_linux64.zip'
+execute  'sudo chmod +x chromedriver'
+execute  'sudo mv chromedriver /usr/local/bin/chromedriver'
 execute  'sudo apt install -y python3-pip'
 # Install app dependencies
 execute 'sudo pip3 install -r /home/projectschool/requirements.txt'
