c5a3e48c2fa838abc0883741c3170209a0305aec
shlomi cohen <100487390+shlomi888c@users.noreply.github.com>
2023-06-12 20:17:30 +0300

    Update default.rb

--- default.rb
+++ default.rb
@@ -3,12 +3,23 @@ execute 'sudo git clone https://shlomi888c@github.com/shlomi888c/projectschool.g
 end
 execute  'sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb'
 execute  'sudo apt install -y ./google-chrome-stable_current_amd64.deb'
-execute  'version=$(curl -s "https://chromedriver.storage.googleapis.com/LATEST_RELEASE")'
-execute  'sudo curl /tmp/ "https://chromedriver.storage.googleapis.com/${version}/chromedriver_linux64.zip"'
-execute  'sudo apt-get install unzip -y'
-execute  'sudo unzip /tmp/chromedriver_linux64.zip'
-execute  'sudo chmod +x chromedriver'
-execute  'sudo mv chromedriver /usr/local/bin/chromedriver'
+package 'unzip'
+version = `curl -s "https://chromedriver.storage.googleapis.com/LATEST_RELEASE"`
+remote_file '/tmp/chromedriver_linux64.zip' do
+  source "https://chromedriver.storage.googleapis.com/#{version}/chromedriver_linux64.zip"
+  mode '0644'
+  action :create
+end
+execute 'unzip_chromedriver' do
+  command 'unzip -o /tmp/chromedriver_linux64.zip -d /usr/bin'
+  action :run
+end
+file '/usr/bin/chromedriver' do
+  mode '0755'
+  action :create
+end
 execute  'sudo apt install -y python3-pip'
 # Install app dependencies
 execute 'sudo pip3 install -r /home/projectschool/requirements.txt'
+
+
