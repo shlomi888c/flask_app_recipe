8a3c23c6138bde7c39dc97c2cf7a87f1b372b599
shlomi cohen <100487390+shlomi888c@users.noreply.github.com>
2023-06-12 18:44:08 +0300

    Update default.rb

--- default.rb
+++ default.rb
@@ -4,7 +4,7 @@ end
 execute  'sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb'
 execute  'sudo apt install -y ./google-chrome-stable_current_amd64.deb'
 execute  'version=$(curl -s "https://chromedriver.storage.googleapis.com/LATEST_RELEASE")'
-execute  'sudo wget -qP /tmp/ "https://chromedriver.storage.googleapis.com/${version}/chromedriver_linux64.zip"'
+execute  'sudo curl /tmp/ "https://chromedriver.storage.googleapis.com/${version}/chromedriver_linux64.zip"'
 execute  'sudo apt-get install unzip -y'
 execute  'sudo unzip /tmp/chromedriver_linux64.zip'
 execute  'sudo chmod +x chromedriver'
